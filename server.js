6371cba8e099fff6f268d67064e45d312efa4ac5
Abdul Samed Alhassan <129804994+abdulsamedtma@users.noreply.github.com>
2023-10-02 16:18:41 -0500

    adssws comments to code in my handlebars & server.js file

--- server.js
+++ server.js
@@ -15,33 +15,32 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Super secret secret', // Secret used to sign the session ID cookie.
   cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
+      maxAge: 300000, // Maximum session age in milliseconds (5 minutes in this case).
+      httpOnly: true, // The cookie is only accessible via HTTP.
+      secure: false, // Insecure cookie (not using HTTPS).
+      sameSite: 'strict', // Cookies are only sent in a first-party context.
   },
-  resave: false,
-  saveUninitialized: true,
+  resave: false, // Do not save session data if not modified.
+  saveUninitialized: true, // Save uninitialized (new) sessions.
   store: new SequelizeStore({
-      db: sequelize
+      db: sequelize // Use Sequelize to store session data in the database.
   })
 };
 
-app.use(session(sess));
+app.use(session(sess)); // Use the session middleware to manage sessions.
 
-// Inform Express.js on which template engine to use
+// Inform Express.js on which template engine to use (Handlebars in this case).
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json()); // Parse JSON request bodies.
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies.
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory.
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));   
-
-app.use(routes);
+app.use(routes); // Use the defined routes.
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}/`));
+  app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}/`)); // Start the server.
 });
